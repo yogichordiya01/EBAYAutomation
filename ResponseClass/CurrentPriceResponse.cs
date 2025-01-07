2ab502f969c3b8fb80b9ff24cdd51879c53bcaef
yogichordiya01 <yogichordiya@gmail.com>
2025-01-07 05:50:47 +0530

    Adding API Automation Code

---
+++ ResponseClass/CurrentPriceResponse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAutomation.ResponseClass
+{
+    public class CurrentPriceResponse
+    {
+        public Time Time { get; set; }
+        public string Disclaimer { get; set; }
+        public string ChartName { get; set; }
+        public Bpi Bpi { get; set; }
+    }
+
+    public class Time
+    {
+        public string Updated { get; set; }
+        public string UpdatedISO { get; set; }
+        public string Updateduk { get; set; }
+    }
+
+    public class Currency
+    {
+        public string Code { get; set; }
+        public string Symbol { get; set; }
+        public string Rate { get; set; }
+        public string Description { get; set; }
+        public double RateFloat { get; set; }
+    }
+
+    public class Bpi
+    {
+        public Currency USD { get; set; }
+        public Currency GBP { get; set; }
+        public Currency EUR { get; set; }
+    }
+
+}
---
+++ TestScripts/CurrentPrice.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using Newtonsoft.Json;
+using System.Net;
+using APIAutomation.ResponseClass;
+
+namespace APIAutomation.TestScripts
+{
+    [TestClass]
+    public class CurrentPrice
+    {
+        private readonly RestClient client;
+        public TestContext TestContext { get; set; }
+
+        public CurrentPrice()
+        {
+            // Initialize RestClient with the base URL of the API
+            client = new RestClient("https://api.coindesk.com");
+        }
+        [TestMethod]
+        public void API_CheckCurrentPrice()
+        {
+            // GET request to specific endpoint
+            var request = new RestRequest(TestContext.Properties["RestEndPoint"].ToString(), Method.Get);            
+            var response = client.Execute(request);     // Execute the request
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);    // Assert status code OK
+
+            // Assert if BPI's are exists or not
+            Assert.IsTrue(response.Content.Split("bpi")[1].Contains(TestContext.Properties["Bpi1Code"].ToString()));
+            Assert.IsTrue(response.Content.Split("bpi")[1].Contains(TestContext.Properties["Bpi2Code"].ToString()));
+            Assert.IsTrue(response.Content.Split("bpi")[1].Contains(TestContext.Properties["Bpi3Code"].ToString()));
+
+            // Deserializing the response
+            CurrentPriceResponse data = JsonConvert.DeserializeObject<CurrentPriceResponse>(response.Content);
+
+            // Perform further assertions on the response data
+            Assert.IsNotNull(data);
+            Assert.AreEqual(TestContext.Properties["GbpDescription"].ToString(), data.Bpi.GBP.Description);
+        }
+    }
+}
